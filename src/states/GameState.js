66589a73a707e31b1a53151717bdab8b81a488d6
Elias Hansen <pe_lias@msn.com>
2016-02-26 16:06:23 +0100

    added firebase

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -2,7 +2,7 @@ import RainbowText from 'objects/RainbowText';
 
 class GameState extends Phaser.State {
 
-	create() {
+ 	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
 		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
 		text.anchor.set(0.5);
