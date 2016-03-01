e0335a2f3fcbadfd333b863874230d71fff87522
earthquaker <pe_lias@msn.com>
2016-03-01 15:41:57 +0100

    changed text in gamestate

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -4,7 +4,7 @@ class GameState extends Phaser.State {
 
  	create() {
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\real time game with Firebase and ES6");
 		text.anchor.set(0.5);
 	}
 
